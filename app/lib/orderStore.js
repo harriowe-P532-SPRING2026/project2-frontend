abe59cdfd0e06498171105be4cb2dd7e5d084d56
Owen Harris <owenh0410@gmail.com>
2026-03-31 15:03:41 -0400

    feat: initial commit

---
+++ app/lib/orderStore.js
@@ -0,0 +1,10 @@
+import { create } from 'zustand'
+
+// const host = "https://triage-api.harrisowe.me"
+const host = "http://localhost:8080"
+
+const useOrderStore = create((set, get) => {
+
+});
+
+export default useOrderStore;
